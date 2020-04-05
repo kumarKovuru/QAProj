0aca53fb5a6e6a5cb519a6938b3699578d67a132
kumarKovuru <58424958+kumarKovuru@users.noreply.github.com>
2020-04-05 11:35:36 +0530

    Test Added

---
+++ SourceCode/eMids.QA.Application.Test/ComponentTestingofPatient.feature
@@ -0,0 +1,48 @@
+﻿Feature: Component testing of patient
+Description:  The purpose of this feature is to create, Read, Update and Delete the patient using Component.
+
+@mytag
+  Scenario:1 CREATE - New Patient created successfully with valid data for all the fields
+    Given user provides First Name as "Jojo"
+    And user provides Last Name as "John"
+    And user provides Member Id as "456" 
+	And user provides Date of Birth as "03-04-20" 
+	And user provides Height Id as "170.12" 
+	And user provides PhoneNumber Id as "1234567890" 
+	And user provides Gender Id as "Male" 
+	And user provides Weight Id as "56.67" 
+	When  User Calls NewPatientRegistration method	
+    Then NewPatientRegistration is successful
+
+	@mytag
+	Scenario:2 GET - Get newly created Patient
+    When user Calls LastCreatedPatient method
+	Then GetLastCreatedPatient is successful
+
+	@mytag
+  Scenario:3 UPDATE - Patient Updated successfully with valid data for all the fields
+    Given user provides First Name as "Jojo Updated"
+    And user provides Last Name as "John Updated"
+    And user provides Member Id as "123" 
+	And user provides Date of Birth as "03-03-20" 
+	And user provides Height Id as "175.12" 
+	And user provides PhoneNumber Id as "1234567" 
+	And user provides Gender Id as "female" 
+	And user provides Weight Id as "50.67" 
+	When  User Calls UpdatePatient method	
+   Then UpdatePatient is successful
+
+   @mytag
+   Scenario:4 GET - Get newly Updated Patient
+    When user Calls LastUpdatedPatient method
+	Then GetLastUpdatedPatient is successful
+
+	@mytag
+    Scenario:5 DELETE - Patient Deleted successfully with Patient Id
+     When user Calls DeletePatient method
+	Then DeletePatient is successful
+
+	@mytag
+	Scenario:6 GET - Get newly Deleted Patient
+    When user Calls LastDeletedPatient method
+	Then GetLastDeletedPatient is successful
---
+++ SourceCode/eMids.QA.Application.Test/TestHooks.cs
@@ -0,0 +1,78 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using System;
+using System.IO;
+using TechTalk.SpecFlow;
+
+namespace eMids.QA.Application.Test
+{
+    [Binding]
+    public class TestHooks
+    {
+        private static ExtentTest featureName;
+        private static ExtentTest scenario;
+        private static ExtentReports extent;
+        private static readonly string PathReport = @"E:\Excelpath\ExtentReport.html";
+        // private readonly ScenarioContext scenarioContext;
+
+        [BeforeFeature]
+        public static void CreateFeature(FeatureContext featureContext)
+        {
+            featureName = extent.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+        }
+
+
+        [BeforeTestRun]
+        public static void InitializeReport()
+        {
+            var solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory));
+            var file = Path.Combine(solutionDir, "../..", "Reports", "ExtentReports", "ExtentReport.html");
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
+
+            var htmlReporter = new ExtentV3HtmlReporter(PathReport);
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+
+
+        [AfterStep]
+        public void InsertReportingSteps(ScenarioContext scenarioContext)
+        {
+            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            if (scenarioContext.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
+            }
+            else if (scenarioContext.TestError != null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+            }
+        }
+
+        [BeforeScenario]
+        public void Initialize(ScenarioContext scenarioContext)
+        {
+            scenario = featureName.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
+        }
+
+        [AfterTestRun]
+        public static void TearDownReport()
+        {
+            extent.Flush();
+        }
+    }
+}
